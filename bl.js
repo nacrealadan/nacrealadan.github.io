b9724c8aab42efb4467de0b807ec6b204fac5cfb
nacrealadan <46814720+nacrealadan@users.noreply.github.com>
2019-06-23 18:20:27 +0300

    Update bl.js

--- bl.js
+++ bl.js
@@ -6,25 +6,9 @@ function bt() {
     return;
   }
   
-  navigator.bluetooth.requestDevice({
-      filters: [{
-        services: ['battery_service']
-      }]
-    })
+  navigator.bluetooth.requestDevice(serviceFilters)
     .then(function (device) {
       return device.gatt.connect();
-    })
-    .then(function (server) {
-      return server.getPrimaryService('battery_service');
-    })
-    .then(function (service) {
-      return service.getCharacteristic('battery_level');
-    })
-    .then(function (characteristic) {
-      return characteristic.readValue();
-    })
-    .then(function (value) {
-      $target.innerHTML = 'Battery percentage is <b>' + value.getUint8(0) + '</b>.';
     })
     .catch(function (error) {
       $target.innerText = error;
